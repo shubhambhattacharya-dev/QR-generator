454eb7fbe5ce70b4bd7a1bc73bfbe61eef665e81
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-02-20 00:00:43 +0530

    qr generator

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hello world");
+import inquirer from 'inquirer';
+import qr from "qr-image"
+import fs, { writeFile } from 'fs';
+
+
+
+inquirer.prompt([{
+    message:"Enter your URL/LINK:",
+    name:"url",
+}
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    const url = answers.url;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_imge.png'));
+
+    fs.writeFile("URL.txt",url, (err)=>{
+        if(err) throw err;
+        console.log("File created");
+    })
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
